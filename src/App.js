439519696153b27a7b90556f3b48287a853654a7
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-03-03 14:35:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.container}>
+      <h1>C'thulhu
+      <img src="https://vignette.wikia.nocookie.net/lovecraft/images/c/cf/Screenshot_20171018-093500.jpg/revision/latest?cb=20171020174137" alt=""/>
+      </h1>
+        <div>
+          <h2>Cell 555-666-777</h2>
+          <h2>Email goo@lovecraft.com</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro eos nam hic. Harum rem dolore totam ea mollitia dolorum eum perspiciatis ullam incidunt numquam, eaque corrupti est cupiditate ex iste!</p>
+        </div>
+        <ul>
+          <li className={classes.cvSegment}>
+            <h3>Education</h3>
+            <div className={classes.detailed}>
+              <h4>WMU - Planet Vhoorl</h4>
+              <h5>Major: Public Relations</h5>
+              <h5>Minor: Scale Tending</h5>
+            </div>
+          </li>
+          <li className={classes.cvSegment}>
+            <h3>Skills</h3>
+            <div className={classes.detailed}>
+              <h4>Office skills</h4>
+              <h5>Office and records smanagement, database administration, event organization, customer suport, travel coordination</h5>
+              <h4>Computer skills</h4>
+              <h5>Microsoft productivity software, Windows</h5>
+            </div>
+          </li>
+          <li className={classes.cvSegment}>
+            <h3>Experience</h3>
+            <div className={classes.detailed}>
+              <h4>Doomsday Cult</h4>
+              <ul>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sint voluptatem ab at quisquam quas dolores, rerum dolore, culpa architecto officiis non asperiores. Voluptate beatae deserunt provident quidem adipisci. Fuga?</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ducimus nesciunt quaerat distinctio nam aliquam, fugit consectetur corporis aspernatur veniam repellat, ratione doloremque unde expedita molestias autem delectus. Quisquam, tempore?</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita repudiandae magnam eos tempora eius dolor earum cum. Commodi sint quaerat repellendus pariatur saepe, sunt at. Inventore asperiores laboriosam perspiciatis doloremque.</li>
+              </ul>
+              <h4>The Watering Hole</h4>
+              <ul>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sint voluptatem ab at quisquam quas dolores, rerum dolore, culpa architecto officiis non asperiores. Voluptate beatae deserunt provident quidem adipisci. Fuga?</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ducimus nesciunt quaerat distinctio nam aliquam, fugit consectetur corporis aspernatur veniam repellat, ratione doloremque unde expedita molestias autem delectus. Quisquam, tempore?</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita repudiandae magnam eos tempora eius dolor earum cum. Commodi sint quaerat repellendus pariatur saepe, sunt at. Inventore asperiores laboriosam perspiciatis doloremque.</li>
+              </ul>
+            </div>
+          </li>
+          <li className={classes.cvSegment}>
+            <h3>Hobbies</h3>
+            <div className={classes.detailed}>
+              <p>World domination, Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores doloribus cum vel quam aperiam minus, voluptatibus mollitia doloremque tempore eligendi, odio expedita harum quis voluptas natus omnis repudiandae excepturi eum.</p>
+            </div>
+          </li>
+          <li className={classes.cvSegment}>
+            <h3>References</h3>
+            <div className={classes.detailed}>
+              <p>Available on request.s</p>
+            </div>
+          </li>
+        </ul>          
     </div>
-  );
+  )
 }
 
 export default App;
